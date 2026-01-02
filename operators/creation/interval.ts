a0426496205ebe338c5e34b3b59e51bdebfa4552
shura <shyrkatawi@gmail.com>
2026-01-03 02:31:38 +0300

    interval

---
+++ operators/creation/interval.ts
@@ -0,0 +1,26 @@
+import { Observable, take } from "rxjs";
+import { observeToConsole } from "../../utils/observe-to-console";
+import { clearInterval } from "node:timers";
+
+const myInterval = (period: number = 0): Observable<number> => {
+  if (period < 0) {
+    period = 0;
+  }
+  return new Observable((subscriber) => {
+    let value = 0;
+
+    const intervalId = setInterval(() => {
+      if (subscriber.closed) {
+        return;
+      }
+      subscriber.next(value);
+      value++;
+    }, period);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  });
+};
+
+observeToConsole(myInterval(1000).pipe(take(4)));
